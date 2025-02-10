0795773520eb3757739fadd9f37731fdc12a2ad8
rinosetiyo <rino.setiyo96@gmail.com>
2025-02-10 23:54:25 +0700

    refactor: update index view to retrieve all posts instead of featured ones

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render # type: ignore
 from blog.models import Post, Comment
 
 # Create your views here.
+
 def index(request):
-    featured = Post.objects.filter(featured_blog=True)[0:2]
+    posts = Post.objects.all()
     context = {
-        'featured': featured,
+        'posts': posts,
     }
     return render(request, 'index.html', context)
